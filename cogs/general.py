98405142d38a164982ca4ca348dc2ad8e4809145
Robert S <admin@robert-s.dev>
2021-06-13 21:02:55 +0200

    Added cog support and added ping command

---
+++ cogs/general.py
@@ -0,0 +1,31 @@
+import os
+
+import discord
+
+from discord.ext import commands
+
+EMB_COLOUR = int(os.getenv("COLOUR"), 16)
+
+
+class General(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        """Pings the bot."""
+
+        embed = discord.Embed(title="Pong :ping_pong:",
+                              description=f"Heartbeat: {round(self.bot.latency * 1000)}ms",
+                              colour=EMB_COLOUR)
+
+        embed.set_footer(text="Wavy • https://wavybot.com",
+                         icon_url=self.bot.user.avatar_url)
+
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    # Add cog to bot
+    bot.add_cog(General(bot))
--- main.py
+++ main.py
@@ -11,6 +11,26 @@ TOKEN = os.getenv("TOKEN")
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix="%", case_insensitive=True, intents=intents)
+bot = commands.Bot(command_prefix="%",
+                   case_insensitive=True,
+                   intents=intents,
+                   status=discord.Status.online,
+                   activity=discord.Game('https://wavybot.com'))
+
+
+@bot.event
+async def on_ready():
+    """Called when the bot is done preparing the data received from Discord."""
+    print(f'Logged in as\n{bot.user.name}\n{bot.user.id}')
+
+
+for cog in os.listdir("./cogs"):
+    if cog.endswith(".py") and not cog.startswith("_"):
+        try:
+            cog = f"cogs.{cog.replace('.py', '')}"
+            bot.load_extension(cog)
+        except Exception as e:
+            print(f"{cog} cannot be loaded.")
+            raise e
 
 bot.run(TOKEN)
