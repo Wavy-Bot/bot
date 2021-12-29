eb2e1e4db103497f4a983f956d17796a3b638f05
Robert S <admin@robert-s.dev>
2021-12-29 17:53:28 +0100

    Make basic bot
    A new adventure begins.

--- README.md
+++ README.md
@@ -1,58 +1,22 @@
-[![Logo Image](https://cdn.wavybot.com/wavy_logo.png)](https://wavybot.com)
-
-![GitHub](https://img.shields.io/github/license/Wavy-Bot/bot?color=%23ED4245&logo=apache&logoColor=white&style=for-the-badge)
+![GitHub](https://img.shields.io/github/license/Wavy-Bot/bot?color=A42E2B&logo=gnu&logoColor=white&style=for-the-badge)
 [![Discord](https://img.shields.io/discord/710436465938530307?color=%235865F2&label=Discord&logo=discord&logoColor=white&style=for-the-badge)](https://discord.wavybot.com)
-
 
 # Wavy
 
-Wavy is an open-source multi-purpose Discord bot built with [Discord.py](https://discordpy.readthedocs.io/en/latest/) and [PostgreSQL](https://www.postgresql.org/).
-
-## How to contribute
-
-Contributing guidelines, issue templates, pull request templates and code of conduct will be added soon.
+Wavy is an open-source multipurpose Discord bot built with [pycord](https://github.com/Pycord-Development/pycord). Wavy is still in development, so use it at your own risk.
 
 ## Tools and services used
 
 <table>
   <tr>
     <td align="center"><a href="https://www.python.org/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png" width="100px;" alt=""/><br /><sub><b>Python 3</b></sub></a><br /></td>
-    <td align="center"><a href="https://www.postgresql.org/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png" width="100px;" alt=""/><br /><sub><b>PostgreSQL</b></sub></a><br /></td>
-    <td align="center"><a href="https://github.com/freyacodes/Lavalink"><img src="https://serux.pro/9e83af1581.png" width="100px;" alt=""/><br /><sub><b>Lavalink (Wavelink)</b></sub></a><br /></td>
-    <td align="center"><a href="https://www.jetbrains.com/pycharm/"><img src="https://i2.wp.com/clay-atlas.com/wp-content/uploads/2019/10/PyCharm_Logo.svg_.png?resize=1024%2C1024&ssl=1" width="100px;" alt=""/><br /><sub><b>Lavalink</b></sub></a><br /></td>
+    <td align="center"><a href="https://www.jetbrains.com/pycharm/"><img src="https://i2.wp.com/clay-atlas.com/wp-content/uploads/2019/10/PyCharm_Logo.svg_.png?resize=1024%2C1024&ssl=1" width="100px;" alt=""/><br /><sub><b>PyCharm</b></sub></a><br /></td>
     <td align="center"><a href="https://deepsource.io/"><img src="https://static.crozdesk.com/web_app_library/providers/logos/000/011/711/original/deepsource-1608196869-logo.png?1608196869" width="100px;" alt=""/><br /><sub><b>Deepsource</b></sub></a><br /></td>
-    <td align="center"><a href="https://some-random-api.ml/"><img src="https://i.ibb.co/Gnc2djX/logo.png" width="100px;" alt=""/><br /><sub><b>Some Random API</b></sub></a><br /></td>
   </tr>
 </table>
 
-## How to use
-### Requirements:
-- A terminal or CMD (if on Windows).
-- [git](https://git-scm.com/downloads)
-- [Python 3.x, tested on 3.8.](https://www.python.org/downloads/)
-- [An app on Discord](https://discord.com/developers/applications) with bot usage enabled
-- [A Lavalink instance](https://github.com/freyacodes/Lavalink)
-- [A PostgreSQL instance](https://www.postgresql.org/)
-- [A Travitia API key](https://discord.com/invite/C98nsXt)
-- [A Sentry application](https://sentry.io)
-
-```bash
-git clone https://github.com/Wavy-Bot/bot.git
-cd bot
-```
-Then copy the `.env.example` file to `.env` and edit it using your preferred text editor.
-
-Afterwards install the required dependencies and start the bot using
-```bash
-python3 -m pip install -r requirements.txt
-python3 main.py
-```
-
-The aforementioned however is only to run the bot. ~~You wil also need some way to configure it, like a web dashboard. Wavy's web dashboard is located [here](https://github.com/Wavy-Bot/website/).~~
-I will be working on a web dashboard located over [here](https://github.com/Wavy-Bot/website/), but for now I have made configuration commands since it was taking too long.
-
-## To-do list
-Moved to [GitHub Projects](https://github.com/Wavy-Bot/bot/projects/1)
+## Where is the old version?
+The old, now depreciated version of Wavy that used Discord.py can be found [here](https://github.com/Wavy-Bot/bot/tree/depreciated). Please do keep in mind that the old and new versions are **not** compatible with each other, nor is the database (the new version uses MongoDB instead of Postgres).
 
 ## License
 
--- main.py
+++ main.py
@@ -2,196 +2,48 @@ import os
 
 import discord
 import asyncio
-import sentry_sdk
+import uvloop
 
-from core import database
-import core.request as core_request
-from discord.ext import commands
-from sanic import Sanic
-from sanic.response import json
+from dotenv import load_dotenv
 
-SENTRY_URL = os.getenv("SENTRY_URL")
+# Only set this to True if this is a production environment.
+production = False
 
-sentry_sdk.init(SENTRY_URL, traces_sample_rate=1.0)
+load_dotenv(".env.production" if production else ".env.development")
 
 
-async def run():
-    """Starts the bot."""
-    token = os.getenv("TOKEN")
-    secret = os.getenv("API_SECRET")
-    db = database.Database()
-    bot = Wavy(db=db)
-    app = Sanic(__name__)
-    emb_colour = int(os.getenv("COLOUR"), 16)
-
-    # NOTE(Robert): A little explanation of all this crap, the commands are here because
-    #               in the class the commands wouldn't load, but outside they would +
-    #               I don't want these commands in a class/cog since I don't want them
-    #               to be unloadable.
-
-    @bot.command(hidden=True)
-    @commands.is_owner()
-    async def load(ctx, extension):
-        """Loads a cog."""
-        bot.load_extension(f'cogs.{extension}')
-        await ctx.send(f"Loaded `{extension}`.")
-
-    @bot.command(hidden=True)
-    @commands.is_owner()
-    async def unload(ctx, extension):
-        """Unloads a cog."""
-        bot.unload_extension(f'cogs.{extension}')
-        await ctx.send(f"Unloaded `{extension}`.")
-
-    @bot.command(hidden=True)
-    @commands.is_owner()
-    async def reload(ctx, extension):
-        """Reloads a cog."""
-        bot.reload_extension(f'cogs.{extension}')
-        await ctx.send(f"Reloaded `{extension}`.")
-
-    # NOTE(Robert): This is here because if I were to put it in a cog it would only apply for said cog,
-    #               not globally.
-
-    @bot.event
-    async def on_message(message):
-        """Called when a message is created and sent."""
-        if isinstance(message.channel,
-                      discord.DMChannel) and not message.author.bot:
-            async with message.channel.typing():
-                res = await core_request.cleverbot(message.content,
-                                                   message.author.id)
-
-                embed = discord.Embed(title="Cleverbot",
-                                      description=res,
-                                      colour=emb_colour)
-
-                embed.set_footer(text="Wavy • https://wavybot.com",
-                                 icon_url=bot.user.avatar_url)
-
-                await message.channel.send(embed=embed)
-        await bot.process_commands(message)
-
-    @app.route("/")
-    async def index(request):
-        """Index page."""
-        return json({"message": "Welcome to Wavy's API."})
-
-    @app.route("/fetch/roles")
-    async def fetch_roles(request):
-        """Fetches all roles of the specified server."""
-        try:
-            guild_id = request.json['guild_id']
-            guild_id = int(guild_id)
-        except TypeError:
-            return json({"message": "No guild ID was provided."}, status=400)
-
-        if request.token != secret:
-            return json(
-                {
-                    "message":
-                    "The API key provided did not match the key that was set."
-                },
-                status=401)
-
-        guild = bot.get_guild(guild_id)
-
-        if not guild:
-            return json({"message": "The guild ID provided was invalid."},
-                        status=400)
-
-        role_list = []
-        roles = guild.roles
-        roles.pop(0)  # Remove @everyone role
-
-        for i in roles:
-            role_list.append({"id": i.id, "name": i.name})
-
-        return json(role_list)
-
-    @app.route("/fetch/channels")
-    async def fetch_channels(request):
-        """Fetches all text channels of the specified server."""
-        try:
-            guild_id = request.json['guild_id']
-            guild_id = int(guild_id)
-        except TypeError:
-            return json({"message": "No guild ID was provided."}, status=400)
-
-        if request.token != secret:
-            return json(
-                {
-                    "message":
-                    "The API key provided did not match the key that was set."
-                },
-                status=401)
-
-        guild = bot.get_guild(guild_id)
-
-        if not guild:
-            return json({"message": "The guild ID provided was invalid."},
-                        status=400)
-
-        channel_list = []
-        channels = guild.text_channels
-
-        for i in channels:
-            channel_list.append({"id": i.id, "name": i.name})
-
-        return json(channel_list)
+async def run(event_loop: uvloop.Loop) -> None:
+    """Runs the bot."""
+    bot = Wavy(event_loop)
 
     try:
-        bot.loop.create_task(
-            app.create_server(host="127.0.0.1",
-                              port=31088,
-                              debug=False,
-                              return_asyncio_server=True))
-
-        await bot.start(token)
+        await bot.start(os.environ["TOKEN"])
     except KeyboardInterrupt:
         await bot.logout()
 
 
-class Wavy(commands.AutoShardedBot):
-    """
-    The blazing-fast Discord bot - now as a class for as little as $69420.99!
-    (this is a joke if you haven't noticed already ^)
-    """
-    def __init__(self, db):
-        self.db = db
+class Wavy(discord.Client):
+    """The blazing-fast Discord bot."""
 
+    def __init__(self, event_loop):
         intents = discord.Intents.default()
         intents.members = True
 
-        super().__init__(command_prefix=self.__fetch_prefix,
-                         case_insensitive=True,
-                         intents=intents,
-                         status=discord.Status.online,
-                         activity=discord.Game('https://wavybot.com'),
-                         help_command=None)
+        super().__init__(
+            command_prefix="%",
+            case_insensitive=True,
+            intents=intents,
+            status=discord.Status.online,
+            activity=discord.Game("https://wavybot.com"),
+            help_command=None,
+            loop=event_loop,
+        )
 
-        for cog in os.listdir("./cogs"):
-            if cog.endswith(".py") and not cog.startswith("_"):
-                try:
-                    cog = f"cogs.{cog.replace('.py', '')}"
-                    self.load_extension(cog)
-                except Exception as e:
-                    print(f"{cog} cannot be loaded.")
-                    raise e
-
-    async def __fetch_prefix(self, botobject, message):
-        """Fetches the bot's prefix from the database."""
-        if not message.guild:
-            return commands.when_mentioned_or("%")(botobject, message)
-
-        prefix = await self.db.fetch_config_prefix(message.guild.id)
-
-        if not prefix:
-            prefix = "%"
-
-        return commands.when_mentioned_or(prefix)(botobject, message)
+    async def on_ready(self) -> print:
+        print(f"Logged in as\n{self.user.name}\n{self.user.id}")
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+    loop = uvloop.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(run(loop))
